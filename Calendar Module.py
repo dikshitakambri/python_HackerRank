79a19881b3e34f3420f757bff262710ad9e8cfff
koustav2001 <89209950+koustav2001@users.noreply.github.com>
2021-10-17 13:00:45 +0530

    Calendar Module.py
    It takes input as a date and gives output which day it is.Thank You.

---
+++ Calendar Module.py
@@ -0,0 +1,6 @@
+import calendar
+
+day = {0:'MONDAY', 1:'TUESDAY', 2:'WEDNESDAY', 3:'THURSDAY', 4:'FRIDAY', 5:'SATURDAY', 6:'SUNDAY'}
+
+m,d,y = map(int,input().split())
+print day[calendar.weekday(y,m,d)]
